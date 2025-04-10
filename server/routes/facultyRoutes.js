1f2831b49b09ff1e1304a0c8f79f563f6fbcc026
suriyaprathap <suriyaprathap01@gmail.com>
2025-04-10 21:37:13 +0530

    Initial Commit

---
+++ server/routes/facultyRoutes.js
@@ -0,0 +1,27 @@
+import express from "express";
+import {
+  facultyLogin,
+  updatedPassword,
+  updateFaculty,
+  createTest,
+  getTest,
+  getStudent,
+  uploadMarks,
+  markAttendance,
+  getAllStudent,
+} from "../controller/facultyController.js";
+import auth from "../middleware/auth.js";
+
+const router = express.Router();
+
+router.post("/login", facultyLogin);
+router.post("/updatepassword", auth, updatedPassword);
+router.post("/updateprofile", auth, updateFaculty);
+router.post("/createtest", auth, createTest);
+router.post("/gettest", auth, getTest);
+router.post("/getstudent", auth, getStudent);
+router.post("/uploadmarks", auth, uploadMarks);
+router.post("/markattendance", auth, markAttendance);
+router.get("/getallstudent", auth, getAllStudent);
+
+export default router;
